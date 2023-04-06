ff36b953b08d6e6537426db5b4a083dba15e5072
CCOSORIOT <127028350+CCOSORIOT@users.noreply.github.com>
2023-04-06 09:53:27 -0500

    Lab 4 - V1
    Lab 4 - V1

--- pregunta.py
+++ pregunta.py
@@ -14,8 +14,17 @@ import pandas as pd
 
 def ingest_data():
 
-    #
-    # Inserte su código aquí
-    #
-
-    return df
+    Datos = pd.read_fwf("clusters_report.txt", skiprows=4, skipfooter=0, names=['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave', 'principales_palabras_clave'])
+    Datos['cluster'] = Datos['cluster'].fillna(method="ffill")
+    Datos['principales_palabras_clave'] = Datos[['cluster','principales_palabras_clave']].groupby(['cluster'])['principales_palabras_clave'].transform(lambda x: ' '.join((x)))
+    Datos = Datos.dropna()
+    Datos = Datos.reset_index()
+    del Datos["index"]
+    Datos = Datos.replace(r'\s+', ' ', regex=True)
+    Datos['principales_palabras_clave'] = Datos['principales_palabras_clave'].str.replace('.','')
+    Datos['porcentaje_de_palabras_clave'] = Datos['porcentaje_de_palabras_clave'].str[:-2]
+    Datos['porcentaje_de_palabras_clave'] = Datos['porcentaje_de_palabras_clave'].str.replace(',','.')
+    Datos['porcentaje_de_palabras_clave'] = pd.to_numeric(Datos['porcentaje_de_palabras_clave'])
+    Datos = Datos.astype({'principales_palabras_clave':'string'})
+    
+    return Datos
